54893a5d8a189ac873cc510ad3ebc86633e9114a
Rob West <robertgregorywest@gmail.com>
2020-04-26 07:51:09 +0100

    Added noopener to footer links

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,11 +3,10 @@ import Box from 'components/box';
 import { Copyright, SocialLink } from './footer.css';
 
 const Footer = () => (
-
   <Box>
-      <SocialLink href="mailto:mj@martinjennings.com" target="_blank"></SocialLink>
-      <SocialLink href="https://twitter.com/mjenningssculpt" target="_blank" platform="twitter"></SocialLink>
-      <Copyright>Copyright 2020 Martin Jennings. Made using <a href="https://kontent.ai">Kentico Kontent</a> and <a href="https://www.gatsbyjs.org/">GatsbyJS</a> by <a href="https://robwest.info">Rob West</a></Copyright>
+      <SocialLink href="mailto:mj@martinjennings.com" target="_blank" rel="noreferrer noopener"></SocialLink>
+      <SocialLink href="https://twitter.com/mjenningssculpt" platform="twitter" target="_blank" rel="noreferrer noopener"></SocialLink>
+      <Copyright>Copyright 2020 Martin Jennings. Made using <a href="https://kontent.ai" target="_blank" rel="noreferrer noopener">Kentico Kontent</a> and <a href="https://www.gatsbyjs.org/" target="_blank" rel="noreferrer noopener">GatsbyJS</a> by <a href="https://robwest.info" target="_blank" rel="noreferrer noopener">Rob West</a></Copyright>
   </Box>
 );
 
