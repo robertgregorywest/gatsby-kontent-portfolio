c97744c12cbafa1b4065d1019b4c839962e34ca9
Rob West <robertgregorywest@gmail.com>
2020-04-30 15:48:58 +0100

    Reformatted footer to fix linting warnings

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,9 +4,15 @@ import { Copyright, SocialLink } from './footer.css';
 
 const Footer = () => (
   <Box>
-      <SocialLink href="mailto:mj@martinjennings.com" target="_blank" rel="noreferrer noopener"></SocialLink>
-      <SocialLink href="https://twitter.com/mjenningssculpt" platform="twitter" target="_blank" rel="noreferrer noopener"></SocialLink>
-      <Copyright>Copyright 2020 Martin Jennings. Made using <a href="https://kontent.ai" target="_blank" rel="noreferrer noopener">Kentico Kontent</a> and <a href="https://www.gatsbyjs.org/" target="_blank" rel="noreferrer noopener">GatsbyJS</a> by <a href="https://robwest.info" target="_blank" rel="noreferrer noopener">Rob West</a></Copyright>
+    <SocialLink 
+      href="mailto:mj@martinjennings.com" 
+      target="_blank" 
+      rel="noreferrer noopener"></SocialLink>
+    <SocialLink href="https://twitter.com/mjenningssculpt" 
+      platform="twitter" 
+      target="_blank" 
+      rel="noreferrer noopener"></SocialLink>
+    <Copyright>Copyright 2020 Martin Jennings. Made using <a href="https://kontent.ai" target="_blank" rel="noreferrer noopener">Kentico Kontent</a> and <a href="https://www.gatsbyjs.org/" target="_blank" rel="noreferrer noopener">GatsbyJS</a> by <a href="https://robwest.info" target="_blank" rel="noreferrer noopener">Rob West</a></Copyright>
   </Box>
 );
 
